05ccb972df64925a6d55e3f07069e6cfa440e6bd
Felipe <hernandezfeli@gmail.com>
2016-07-15 13:35:49 -0300

    Commit inicial

---
+++ CaptchaBreaker.rb
@@ -0,0 +1,180 @@
+require 'RMagick'
+require 'rtesseract'
+
+class CaptchaBreaker
+  def initialize(image_file, index)
+    @image = Magick::Image.read(image_file).first
+    @index = index.to_s
+  end
+
+  def break
+    #borro la barra gris
+    @image = @image.opaque_channel('#5e5e5e', 'white', invert=false, fuzz=10000 )
+
+    #paso a gray
+    @image = @image.quantize(3, Magick::GRAYColorspace)
+
+    #completo barra borrada
+    @image = complete_graybar(@image)
+
+
+    #dejo el times para ir probando con algunos pasos más
+    @image = erode(@image)
+    @image = erode(@image, :inflate)
+    @image = remove_blacks(@image)
+
+    #exporto la imagen
+    @image.write("to_blob" + @index + ".png")
+
+    # Use tesseract to read the characters
+    @image.format = 'JPEG'
+    tesseract = RTesseract.new('options: :digits')
+    tesseract.from_blob @image.to_blob
+
+    #armo array
+    captcha = tesseract.to_s_without_spaces.split(//)
+
+    #elimino todo lo que no es numero
+    captcha.each_with_index do |value, i|
+      if value == '0' || value == '1' || value == '2' || value == '3' || value == '4' || value == '5' || value == '6' || value == '7' || value == '8' || value == '9'
+        captcha[i] = value
+      else
+        captcha[i] = ""
+      end
+    end
+
+    #paso a string
+    captcha = captcha.join
+
+    #si no tengo 6 numeros, le paso otro filtro para inflar y vuelvo a reconocer
+    if captcha.length != 6
+      1.times { @image = erode(@image) }
+      @image = remove_blacks(@image)
+      1.times { @image = erode(@image, :inflate) }
+
+
+      #exporto la imagen
+      @image.write("to_blob" + @index + "_2.png")
+
+      # Use tesseract to read the characters
+      @image.format = 'JPEG'
+      tesseract = RTesseract.new('options: :digits')
+      tesseract.from_blob @image.to_blob
+      captcha = tesseract.to_s_without_spaces.split(//)
+
+      captcha.each_with_index do |value, i|
+        if value == '0' || value == '1' || value == '2' || value == '3' || value == '4' || value == '5' || value == '6' || value == '7' || value == '8' || value == '9'
+          captcha[i] = value
+        else
+          captcha[i] = ""
+        end
+      end
+      captcha = captcha.join
+    end
+
+    #muestro
+    return captcha
+  end
+
+  private
+
+  def get_pixels(image)
+    image.dispatch(0, 0, image.columns, image.rows, 'R')
+  end
+
+  def image_from_pixels(pixels)
+    pixels = pixels.map{ |px| [px,px,px] }.flatten # Replicate channels to create an rgb image
+    Magick::Image.constitute(@image.columns, @image.rows, 'RGB', pixels)
+  end
+
+  def erode(image, action = :erode)
+    pixels = get_pixels(image)
+
+    if action == :erode
+      white = pixels.uniq.sort.last
+    else
+      white = pixels.uniq.sort.first
+    end
+
+    pixels.each_with_index do |px, i|
+      next if px == white # skip white pixels
+      pixels[i] = 1 if  pixels[i + 1] == white && pixels[i - 1] == white ||
+                        pixels[i + image.columns] == white && pixels[i - image.columns] == white ||
+                        pixels[i + 1] == white && pixels[i + image.columns] == white ||
+                        pixels[i - 1] == white && pixels[i - image.columns] == white
+    end
+    pixels.each_with_index do |px, i|
+      pixels[i] = white if px == 1
+    end
+
+    image_from_pixels(pixels)
+  end
+
+  def complete_graybar(image)
+    #intento completar el espacio vaciodo por la barra gris
+    pixels = get_pixels(image)
+    black = pixels.uniq.sort.first
+    i = 1800
+    gris = 30000
+    110.times {
+      cerca = 0
+      if pixels[i - 120] == black
+        cerca = cerca + 1
+      end
+      if pixels[i - 240] == black
+        cerca = cerca + 1
+      end
+      if pixels[i + 240] == black
+        cerca = cerca + 1
+      end
+      if pixels[i + 360] == black
+        cerca = cerca + 1
+      end
+      if cerca > 2
+        pixels[i] = black
+        pixels[i + 120] = black
+      end
+      i = i + 1
+    }
+    image_from_pixels(pixels)
+
+  end
+
+  def remove_blacks(image)
+    #borramos los puntos negros que quedan sueltos
+    pixels = get_pixels(image)
+    black = pixels.uniq.sort.last
+
+    pixels.each_with_index do |px, i|
+      cerca = 0
+      if pixels[i - 119] == black
+        cerca = cerca + 1
+      end
+      if pixels[i - 120] == black
+        cerca = cerca + 1
+      end
+      if pixels[i - 121] == black
+        cerca = cerca + 1
+      end
+      if pixels[i + 30] == black
+        cerca = cerca + 1
+      end
+      if pixels[i - 30] == black
+        cerca = cerca + 1
+      end
+      if pixels[i + 119] == black
+        cerca = cerca + 1
+      end
+      if pixels[i + 120] == black
+        cerca = cerca + 1
+      end
+      if pixels[i + 121] == black
+        cerca = cerca + 1
+      end
+      pixels[i] = black if cerca > 7
+    end
+    image_from_pixels(pixels)
+
+  end
+
+end
---
+++ test.rb
@@ -0,0 +1,25 @@
+require 'cutest'
+load 'CaptchaBreaker.rb'
+
+solutions = ['402417','014530','670850','789445','970314','861000','760449','960827','053960','822458','213760','501922','416964','123954','397451','165837','654348','787814','694421','507188','598529','399263','869895','538425']
+
+test "solves sample1 image" do
+  ok = 0
+  solutions.each_with_index do |value, i|
+    image = "images_case1/" + (i+1).to_s + ".jpg"
+    solution = CaptchaBreaker.new(image, i).break
+    if solution == value
+      ok = ok + 1
+      print "| " + (i).to_s + " - "
+      print solution
+      print " - ok |"
+    else
+      print "| " + (i).to_s + " - "
+      print solution
+      print " - fail |"
+    end
+    #assert_equal(value, solution)
+  end
+  confianza = (ok * 100 / 24)
+  print "%" + confianza.to_s + "%"
+end
